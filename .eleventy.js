764b22175cfd1758180bd24b5cfb0f7ad7ffe1cf
Lukundu Lako <lukundu.lako@gmail.com>
2020-07-09 21:33:31 -0400

    corrected loading and menu

--- .eleventy.js
+++ .eleventy.js
@@ -3,23 +3,24 @@ const fs = require("fs");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = (eleventyConfig)=>{
-    eleventyConfig.setLiquidOptions({
-        dynamicPartials: true,
-        strict_filters: true,
-        root: ["_includes"]
-    });
     eleventyConfig.addPlugin(pluginNavigation);
+    eleventyConfig.setDataDeepMerge(true);
     eleventyConfig.setTemplateFormats([
+        "njk",
         "md",
         "css",
         "jpg",
         "png",
         "gif"
     ]);
+    
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
+
      /* Markdown Overrides */
     let markdownLibrary = markdownIt({
         html: true,
@@ -32,37 +33,47 @@ module.exports = (eleventyConfig)=>{
     });
     eleventyConfig.setLibrary("md", markdownLibrary);
 
+    // Browsersync Overrides
     eleventyConfig.setBrowserSyncConfig({
         callbacks: {
             ready: function(err, browserSync) {
-            const content_404 = fs.readFileSync('_site/404.html');
+                const content_404 = fs.readFileSync('_site/404.html');
 
-            browserSync.addMiddleware("*", (req, res) => {
+                browserSync.addMiddleware("*", (req, res) => {
                 // Provides the 404 content without redirect.
                 res.write(content_404);
                 res.end();
-            });
+                });
             },
         },
         ui: false,
         ghostMode: false
     });
 
+    eleventyConfig.addPlugin(lazyImagesPlugin);
     eleventyConfig.addCollection('albums', collection => {
         return [
-          ...collection.getFilteredByGlob('./albums/**/*.md')
+          ...collection.getFilteredByGlob('./albums/**/*.njk')
         ].reverse();
     });
 
     return {
         templateFormats: [
             "md",
-            "ejs",
-            "css",
-            "svg",
-            "png",
+            "njk",
+            "html",
+            "liquid"
         ],
-        passthroughFileCopy: true
-      };
-    
+        markdownTemplateEngine: "liquid",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+
+        // These are all optional, defaults are shown:
+        dir: {
+            input: ".",
+            includes: "_includes",
+            data: "_data",
+            output: "_site"
+        }
+    };
 };
