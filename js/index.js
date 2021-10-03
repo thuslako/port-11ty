6bfac8b110696423308e31f0ca41950080b18fb0
Luk <luk@Lukundus-MacBook-Air.local>
2021-10-03 19:07:43 -0400

    update: copy email functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+const copyText = (text)=>{
+  const content = text.getAttribute('data-copy')
+  if(content){
+    navigator.clipboard.writeText(content);
+    text.children[0].classList.add("show");
+    setTimeout(()=>{text.children[0].classList.toggle("show");},1500)
+  }
+  else{
+    return
+  }
+}
+
 (()=>{
   console.log('My portfolio')
 })()
