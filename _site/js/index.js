329742c0012743f2d3dc47ea611dd49d25f7b86e
Lukundu Lako <lukundulako@Lukundus-Mac-Studio.local>
2022-09-23 00:10:21 -0400

    update: new scroll

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,40 +1,17 @@
 gsap.registerPlugin(ScrollTrigger);
-let maxWidth = 0;
 const slides = gsap.utils.toArray(".slide");
 const gallery = document.getElementById("_gallery");
-const getMaxWidth = () => {
-  maxWidth = 0;
-  slides.forEach((slide) => {
-    maxWidth += slide.offsetWidth;
-  });
-};
-getMaxWidth();
-ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
 
 gsap.to(slides, {
-  x: () => `-${maxWidth - gallery.offsetWidth}`,
+  xPercent: -100 * (slides.length - 1),
   ease: "none",
   scrollTrigger: {
     trigger: ".gallery",
     pin: true,
-    scrub: true,
-    span: 1 / (slides.length - 1),
-    end: () => `+=${maxWidth}`,
-    invalidateOnRefresh: true,
+    pinSpacing: "margin",
+    scrub: 0.1,
+    end: `+=${gallery.offsetWidth}`,
   },
-});
-
-slides.forEach((slide, i) => {
-  ScrollTrigger.create({
-    trigger: slide,
-    start: () =>
-      "top top-=" +
-      (slide.offsetLeft - gallery.offsetWidth / 2) *
-        (maxWidth / (maxWidth - gallery.offsetWidth)),
-    end: () =>
-      "+=" + slide.offsetWidth * (maxWidth / (maxWidth - gallery.offsetWidth)),
-    toggleClass: { targets: slide, className: "active" },
-  });
 });
 
 const copyText = (text) => {
