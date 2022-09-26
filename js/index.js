fd38f63a000ca75fb41a75ab9668b386c96407e4
Lukundu Lako <lukundulako@Lukundus-Mac-Studio.local>
2022-09-25 22:48:38 -0400

    update: working slide

--- js/index.js
+++ js/index.js
@@ -1,17 +1,40 @@
 gsap.registerPlugin(ScrollTrigger);
 const slides = gsap.utils.toArray(".slide");
 const gallery = document.getElementById("_gallery");
-
 gsap.to(slides, {
   xPercent: -100 * (slides.length - 1),
   ease: "none",
+  y: "0",
   scrollTrigger: {
     trigger: ".gallery",
+    start: "center 50%",
+    end: "bottom top",
     pin: true,
     pinSpacing: "margin",
-    scrub: 0.1,
+    scrub: 1,
+    snap: {
+      snapTo: 1 / (slides.length - 1),
+      duration: 0.02,
+      ease: "power2.inOut",
+    },
     end: `+=${gallery.offsetWidth}`,
   },
+});
+slides.forEach((slide, i) => {
+  ScrollTrigger.create({
+    trigger: slide,
+    start: () =>
+      `top top-=${
+        (slide.offsetLeft - window.innerWidth / 2) *
+        (gallery.offsetWidth / (gallery.offsetWidth - window.innerWidth))
+      }`,
+    end: () =>
+      `+=${
+        slide.offsetWidth *
+        (gallery.offsetWidth / (gallery.offsetWidth - window.innerWidth))
+      }`,
+    toggleClass: { targets: slide, className: "active" },
+  });
 });
 
 const copyText = (text) => {
