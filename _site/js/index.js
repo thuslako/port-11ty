587756989b24548507f796ac3d8174d0f5c2b363
Lukundu Lako <luk@Lukundus-MacBook-Pro.local>
2022-10-13 11:28:58 -0400

    update: new copy function

--- _site/js/index.js
+++ _site/js/index.js
@@ -1,81 +1,15 @@
-gsap.registerPlugin(ScrollTrigger);
-const slides = gsap.utils.toArray(".slide");
-const gallery = document.getElementById("_gallery");
-const album = gsap.to(slides, {
-  xPercent: -100 * (slides.length - 1),
-  ease: "none",
-  y: "0",
-  scrollTrigger: {
-    trigger: ".gallery",
-    start: "center 50%",
-    end: "bottom top",
-    pin: true,
-    pinSpacing: "margin",
-    scrub: 0.3,
-    snap: {
-      snapTo: 1 / (slides.length - 1),
-      duration: 0.02,
-      ease: "power2.inOut",
-    },
-    end: `+=${gallery.offsetWidth}`,
-  },
-});
-slides.forEach((slide, i) => {
-  gsap.set(slide.children[0], {
-    css: {
-      visibility: "hidden",
-      width: "20%",
-      height: "20%",
-      "border-radius": "100%",
-    },
-    scale: 0.1,
-  });
-  gsap.to(slide.children[0], {
-    css: {
-      visibility: "visible",
-      width: "100%",
-      height: "100%",
-      "border-radius": "0%",
-    },
-    scale: 1,
-    duration: 1.5,
-    scrollTrigger: {
-      trigger: slide,
-      containerAnimation: album,
-      start: "left center",
-      toggleActions: "play none none reset",
-      id: i,
-    },
-  });
-  ScrollTrigger.create({
-    trigger: slide,
-    containerAnimation: album,
-    marker: true,
-    start: () =>
-      `top top-=${
-        (slide.offsetLeft - window.innerWidth / 2) *
-        (gallery.offsetWidth / (gallery.offsetWidth - window.innerWidth))
-      }`,
-    end: () =>
-      `+=${
-        slide.offsetWidth *
-        (gallery.offsetWidth / (gallery.offsetWidth - window.innerWidth))
-      }`,
-    toggleClass: { targets: slide, className: "active" },
-  });
-});
-
-const copyText = (text) => {
-  const content = text.getAttribute("data-copy");
-  if (content) {
+(() => {
+  const copyText = (btn) => {
+    const content = btn.target.getAttribute("data-copy");
+    if (!content) return;
     navigator.clipboard.writeText(content);
-    text.children[0].classList.add("show");
+    btn.target.children[0].classList.add("show");
     setTimeout(() => {
-      text.children[0].classList.toggle("show");
-    }, 500);
-  } else {
-    return;
-  }
-};
-
-console.log("My portfolio");
+      btn.target.children[0].classList.toggle("show");
+    }, 2000);
+  };
+  document
+    .getElementById("copy-email")
+    .addEventListener("click", copyText, false);
+  console.log("My portfolio");
+})();
