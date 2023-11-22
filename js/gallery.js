39e548cfc4733271dca99bd516204f66b70abe69
Luk <lukundu.lako@gmail.com>
2023-11-21 21:59:43 -0800

    rework: update gallery class

--- js/gallery.js
+++ js/gallery.js
@@ -6,7 +6,6 @@ class Lightbox {
     this.lightbox = document.getElementById("lightbox");
     this.slides = [...document.getElementsByClassName("slide")];
     this.slideImages = new Map();
-    this.currentImage = null;
     this.lightbox.addEventListener("click", (e) => {
       const lightboxImg = document.querySelector("#lightbox img");
       if (e.target === lightboxImg) return;
@@ -14,55 +13,62 @@ class Lightbox {
       this.lightbox.innerHTML = "";
       document.body.classList.remove("no-scroll");
     });
-    document.addEventListener("keyup", (e) => {
-      if (e.key === "Escape") {
-        this.lightbox.classList.remove("lightbox-open");
-        this.lightbox.innerHTML = "";
-        document.body.classList.remove("no-scroll");
-      }
-    });
   }
 
   init() {
-    if (this.slides.length) {
-      this.slides.shift();
-      for (let i = 0; i < this.slides.length; i++) {
-        const slide = this.slides[i];
-        const images = slide.getElementsByTagName("img");
-        const image = images[0];
-        const imageID = this.getfileId(image.currentSrc);
-        if(imageID){
-            this.slideImages.set(imageID, {
-            next: (i == (this.slides.length-1)? this.slides[0]: this.slides[i+1]),
-            prev: (i == 0? this.slides[this.slides.length-1]: this.slides[i-1])
-          });
-        }
-        image.addEventListener("click", (e) => {
-          e.preventDefault();
-          const lightboxContent = document.createElement("div");
-          lightboxContent.classList.add("lightbox-content");
-          const lightboxClose = document.createElement("button");
-          lightboxClose.classList.add("lightbox-close");
-          lightboxClose.innerHTML = `<ion-icon name="close"></ion-icon>`;
-          const lightboxImg = document.createElement("img");
-          this.lightbox.classList.add("lightbox-open");
-          document.body.classList.add("no-scroll");
-          lightboxImg.src = image.currentSrc;
-          this.currentImage = imageID;
-          lightboxContent.appendChild(lightboxImg);
-          this.lightbox.appendChild(lightboxClose);
-          this.lightbox.appendChild(lightboxContent);
-        });
-        
-      }
-    }
+    this.slides = this.slides.slice(1);
     this.navigate();
+    this.addListeners();
+  }
+
+  toggleInfoBtn() {
+    const galleryInfobtns = document.getElementsByClassName("info-btn");
+    if (galleryInfobtns.length)
+      galleryInfobtns.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+          e.preventDefault();
+          e.target.classList.add("info-btn-open");
+          setTimeout(() => {
+            e.target.classList.remove("info-btn-open");
+          }, 2000);
+        });
+      });
+  }
+
+  addListeners(){
+    if (!this.slides.length) return;
+    for (let i = 0; i < this.slides.length; i++) {
+      const slide = this.slides[i];
+      const images = slide.getElementsByTagName("img");
+      const image = images[0];
+      const imageID = this.getfileId(image.currentSrc);
+      if (imageID) this.slideImages.set(imageID, {
+        next: (i == (this.slides.length - 1) ? this.slides[0].querySelector("img").currentSrc : this.slides[i + 1].querySelector("img").currentSrc),
+        prev: (i == 0 ? this.slides[this.slides.length - 1].querySelector("img").currentSrc : this.slides[i - 1].querySelector("img").currentSrc)
+      });
+      image.addEventListener("click", (e) => {
+        e.preventDefault();
+        const lightboxContent = document.createElement("div");
+        lightboxContent.classList.add("lightbox-content");
+        const lightboxClose = document.createElement("button");
+        lightboxClose.classList.add("lightbox-close");
+        lightboxClose.innerHTML = `<ion-icon name="close"></ion-icon>`;
+        const lightboxImg = document.createElement("img");
+        this.lightbox.classList.add("lightbox-open");
+        document.body.classList.add("no-scroll");
+        lightboxImg.src = image.currentSrc;
+        lightboxContent.appendChild(lightboxImg);
+        this.lightbox.appendChild(lightboxClose);
+        this.lightbox.appendChild(lightboxContent);
+      });
+
+    }
   }
 
 
   navigate() {
     //case switch for document key events
-    document.addEventListener('keydown',(e) => {
+    document.addEventListener('keydown', (e) => {
       e.preventDefault();
       switch (e.key) {
         case "ArrowRight":
@@ -79,27 +85,23 @@ class Lightbox {
   }
   getfileId(url) {
     const regex = /\/assets\/(.*)-\d+\.webp/;
-    const id = [...(url.match(regex))];
-    return id.length? id[1] : null;
+    let id = url.match(regex)
+    return id ? id[1] : false;
   }
 
   gallery(direction) {
-    const _images = this.slideImages
     const lightboxImg = document.querySelector("#lightbox img");
-
     const _currentImage = this.getfileId(lightboxImg.src);
-    if(this.currentImage &&_images.has(_currentImage)){
-      const _image = _images.get(_currentImage);
-      let nextImage;
+    if (this.slideImages.has(_currentImage)) {
+      const _image = this.slideImages.get(_currentImage);
       switch (direction) {
         case 'right':
-          nextImage = _image.next;
+          lightboxImg.src = _image.next;
           break;
         case 'left':
-          nextImage = _image.prev;
+          lightboxImg.src = _image.prev;
           break;
       }
-      lightboxImg.src = nextImage.querySelector("img").currentSrc;
     }
   }
   closeLightbox() {
--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import Lightbox from "./gallery.js";
+
 (function () {
   const copyText = (btn) => {
     const content = btn.target.getAttribute("data-copy");
@@ -13,22 +14,8 @@ import Lightbox from "./gallery.js";
   if (copyEmail) copyEmail.addEventListener("click", copyText, false);
 
   // toggle gallery info button
-  const galleryInfobtns = document.getElementsByClassName("info-btn");
-  if (galleryInfobtns.length)
-    galleryInfobtns.forEach((btn) => {
-      btn.addEventListener("click", (e) => {
-        e.preventDefault();
-        e.target.classList.add("info-btn-open");
-        setTimeout(() => {
-          e.target.classList.remove("info-btn-open");
-        }, 2000);
-      });
-    });
 
-  const lightbox = document.getElementById("lightbox");
-  const slides = [...document.getElementsByClassName("slide")];
-  if (lightbox && slides.length) {
     const lightbox = new Lightbox();
+    lightbox.toggleInfoBtn();
     lightbox.init();
-  }
 })();
